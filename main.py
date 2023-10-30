d439e8edf1ef99c0e80c7463c29fab67d8d3df3e
Will Walters <willwalters3454@gmail.com>
2023-10-29 22:54:15 -0400

    Lots of changes

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 #from pandas_profiling import ProfileReport
 import itertools
+import pickle
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.sequence import TimeseriesGenerator
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, OneHotEncoder
+import tensorflow as tf
 
 
 
@@ -98,53 +103,179 @@ def get_cov_matrix(df):
     for cluster in split_data.keys():
         curr = split_data[cluster]
 
+def make():
+    pass
+
+
+def startup_01():
+    e_df = df_train.dropna(axis=1, how='any').reset_index(drop=True)
+    hier_dict = create_hier_cluster_store(e_df)
+    hk = hier_dict.keys()
+    sales_df_dict = dict()
+    sales_family_dict = dict()
+    # Each store also has sales for each family of product, lets investigate these separate families to solve for each
+    for cluster in hk:
+        cl_list = hier_dict[cluster]
+        sales_family_dict[cluster] = dict()
+        for store in cl_list:
+            store_nbr = store.loc[:, 'store_nbr'].values[0]
+            temp_df = store.groupby('family')
+            sales_family_dict[cluster][store_nbr] = dict()
+            # print(temp_df.head())
+            for f in list(store.loc[:, 'family'].unique()):
+                g = temp_df.get_group(f)
+                g.drop_duplicates(subset=['date'], keep='first', inplace=True)
+                ra = pd.date_range(start=g['date'].min(), end=g['date'].max())
+                missing_dates = ra.difference(g['date'])
+                print(missing_dates)
+                name = str(f).replace('/','&') + "_sales_" + str(store_nbr)+ "_"+str(cluster) +"_01" + ".parquet"
+                print(name)
+                sales_family_dict[cluster][store_nbr][f] = name
+                g.to_parquet(name)
+    with open('01_df_arch.pickle', 'wb') as handle:
+        pickle.dump(sales_family_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+
+def read_01():
+    df_dict = dict()
+    for file in os.listdir('.'):
+        if file.endswith('01.parquet'):
+            print(file)
+            df_dict[file] = pd.read_parquet(file)
+    return df_dict
+
+
+# modeling
+
+def each_family_total_df(cfg, df_dict):
+    each_family = dict()
+    for c in cfg.keys():
+        for s in cfg[c].keys():
+            for f in cfg[c][s].keys():
+                curr_df = df_dict[cfg[c][s][f]].reset_index(drop=True)
+                if f in each_family:
+                    each_family[f] = ((pd.concat([each_family[f], curr_df])).sort_values(by=['date'],ascending=False)).reset_index(drop=True)
+                else:
+                    each_family[f] = (curr_df.sort_values(by=['date'], ascending=False)).reset_index(drop=True)
+    return each_family
+
+def lstm_trainer(df, w, batch):
+    df.drop(columns=['id', 'family', 'city', 'state', 'date'], inplace=True)
+    # store_nbr, onpromotion, store_type, cluster ; sales
+    convert_dict = {'store_nbr':'category','onpromotion':'category','store_type':'category','cluster':'category','sales':float}
+    df = df.astype(convert_dict)
+    df = OneHotEncoder().fit_transform(df)
+    df = MinMaxScaler().fit_transform(df)
+    train_X, test_X, train_y, test_y = train_test_split(df, df[:,'sales'], test_size=0.2, random_state=12, shuffle=False)
+    train_generator = TimeseriesGenerator(train_X, train_y,length=w,sampling_rate=1,batch_size=batch)
+    test_generator = TimeseriesGenerator(test_X, test_y, length=w, sampling_rate=1, batch_size=batch)
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.LSTM(128, input_shape=(w, 5), return_sequences=True))
+    model.add(tf.keras.layers.LeakyReLU(alpha=0.5))
+    model.add(tf.keras.layers.LSTM(128, return_sequences=True))
+    model.add(tf.keras.layers.LeakyReLU(alpha=0.5))
+    model.add(tf.keras.layers.Dropout(0.3))
+    model.add(tf.keras.layers.LSTM(64, return_sequences=True))
+    model.add(tf.keras.layers.Dropout(0.3))
+    model.add(tf.keras.layers.Dense(1))
+    model.summary()
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                                      patience=2,
+                                                      mode='min')
+    model.compile(loss=tf.losses.MeanSquaredError(),
+                  optimizer=tf.optimizers.Adam(),
+                  metrics=[tf.metrics.MeanSquaredError()])
+    history = model.fit_generator(train_generator, epochs=50,
+                                  validation_data=test_generator, shuffle=False,
+                                  callbacks=[early_stopping])
+    model.evaluate_generator(test_generator, verbose=0)
+    predictions = model.predict_generator(test_generator)
+    print(predictions.shape[0])
+
+
+
+
 
 if __name__ == "__main__":
 
     # preprocess()
-
-    df_train = pd.read_parquet('df_train.parquet')
-    df_test = pd.read_parquet('df_test.parquet')
-
-    print(df_train.describe())
-    print(df_test.describe())
-
-    # First let's do EDA of df_train
-    print("Columns")
-    print(df_train.columns)
-    print("Shape")
-    print(df_train.shape)
-    print("Date range")
-    dt1 = pd.to_datetime(max(df_train.loc[:, 'date']))
-    dt2 = pd.to_datetime(min(df_train.loc[:, 'date']))
-    datediff = (dt1 - dt2).days
-    print(dt2)
-    print(dt1)
-    print("Days between")
-    print(datediff)
-
-    print(df_train.isnull().sum())
-    print(df_train.shape)
+    #
+    # df_train = pd.read_parquet('df_train.parquet').set_index('date',drop=False)
+    # df_test = pd.read_parquet('df_test.parquet').set_index('date',drop=False)
+    #
+    # print(df_train.describe())
+    # print(df_test.describe())
+    #
+    # # First let's do EDA of df_train
+    # print("Columns")
+    # print(df_train.columns)
+    # print("Shape")
+    # print(df_train.shape)
+    # print("Date range")
+    # dt1 = pd.to_datetime(max(df_train.loc[:, 'date']))
+    # dt2 = pd.to_datetime(min(df_train.loc[:, 'date']))
+    # datediff = (dt1 - dt2).days
+    # print(dt2)
+    # print(dt1)
+    # print("Days between")
+    # print(datediff)
+    #
+    # print(df_train.isnull().sum())
+    # print(df_train.shape)
     # Drop columns that have nas, we can impute and use later
-    e_df = df_train.dropna(axis=1, how='any')
-    hier_dict = create_hier_cluster_store(e_df)
-    hk = hier_dict.keys()
-    sales_df_dict = dict()
-    for i in hk:
-        stores_df = hier_dict[i]
-        temp_dict = dict()
-        for s in stores_df:
-            store_nbr = s.loc[:, "store_nbr"].values[0]
-            ssales = (pd.Series(s.loc[:,'sales'])).reset_index(drop=True)
-            temp_dict[('storenum_'+str(store_nbr))] = ssales
-            if 'date' not in temp_dict.keys():
-                temp_dict['date'] = pd.Series(s.loc[:,'date'])
-            
-        temp_df = pd.DataFrame(temp_dict)
-        temp_df.set_index('date', inplace=True)
-        sales_df_dict[i] = temp_df
-        print(temp_df.corr())
-    print(sales_df_dict[1].shape)
+    #startup_01()
+    dfs_config = None
+    with open('01_df_arch.pickle', 'rb') as handle:
+        dfs_config = pickle.load(handle)
+
+    df_dict = read_01()
+    # for c in dfs_config.keys():
+    #     for s in dfs_config[c].keys():
+    #         for f in dfs_config[c][s].keys():
+    #             df = df_dict[dfs_config[c][s][f]]
+    #             print(dfs_config[c][s][f])
+    #             print(df.columns)
+    #             print((df.loc[:, 'sales']).describe())
+    # print(len(dfs_config.keys()))
+    # print(len(dfs_config[1].keys()))
+    # for i in dfs_config[1].keys():
+    #     print(len(dfs_config[1][i].keys()))
+    print(df_dict['BABY CARE_sales_51_17_01.parquet'].describe(include='all'))
+    each_fam = each_family_total_df(dfs_config,df_dict)
+    for i in each_fam.keys():
+        print(i)
+        lstm_trainer(each_fam[i], 7, 32)
+
+
+
+
+
+
+
+
+    # Found some duplicated dates in dataset sales_family_dict
+    # Currently is data stucture dict(cluster:dict(store:dict(family_sales:dataframe)))
+
+
+
+
+
+    # for i in hk:
+    #     stores_df = hier_dict[i]
+    #     temp_dict = dict()
+    #     for s in stores_df:
+    #         store_nbr = s.loc[:, "store_nbr"].values[0]
+    #         ssales = (pd.Series(s.loc[:,'sales'])).reset_index(drop=True)
+    #         temp_dict[('storenum_'+str(store_nbr))] = ssales
+    #         if 'date' not in temp_dict.keys():
+    #             temp_dict['date'] = pd.Series(s.loc[:,'date'])
+    #
+    #     temp_df = pd.DataFrame(temp_dict)
+    #     temp_df.set_index('date', inplace=True)
+    #     sales_df_dict[i] = temp_df
+    #     print(temp_df.corr())
+    # print(sales_df_dict[1].shape)
 
     # for i in list(itertools.combinations(hk, 2)):
     #     print(i)
